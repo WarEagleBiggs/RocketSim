1e7f59a4247a79e804d94cd9d081f595cb3eb077
Adam Biggs <wareaglebiggs@gmail.com>
2024-06-12 22:34:13 -0500

    added velocity detection

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -1,29 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Master : MonoBehaviour
 {
     public GameObject Rocket1;
-
     private Rigidbody Rocket1_rb;
-
     public float UpwardVelocity;
+    public Vector3 wind;
+    public TextMeshProUGUI Velocity;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         Rocket1_rb = Rocket1.GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        Rocket1_rb.AddForce(wind);
+        Velocity.SetText(Rocket1_rb.velocity.magnitude.ToString("0" + " m/s"));
     }
 
     public void RunSim()
     {
+        
         Rocket1_rb.AddForce(transform.up * UpwardVelocity, ForceMode.Impulse);
     }
 }
