91787edcece333cd3ad66e13a7fcb321070b535b
Adam Biggs <wareaglebiggs@gmail.com>
2024-06-12 22:05:44 -0500

    Added logic for ship to fly

--- Assets/Scripts/Master.cs
+++ Assets/Scripts/Master.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Master : MonoBehaviour
 {
+    public GameObject Rocket1;
+
+    private Rigidbody Rocket1_rb;
+
+    public float UpwardVelocity;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rocket1_rb = Rocket1.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RunSim()
+    {
+        Rocket1_rb.AddForce(transform.up * UpwardVelocity, ForceMode.Impulse);
     }
 }
