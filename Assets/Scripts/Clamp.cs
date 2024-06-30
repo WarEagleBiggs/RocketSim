aad1fa0bdd092d4cbe8fa6a3e4af087cff6d9850
Adam Biggs <wareaglebiggs@gmail.com>
2024-06-30 17:23:58 -0500

    Added clamp

--- Assets/Scripts/Clamp.cs
+++ Assets/Scripts/Clamp.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Clamp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public bool canExecute;
+    public GameObject refernce;
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(refernce.transform.position, transform.TransformDirection(Vector3.back), out hit) && canExecute)
+        {
+            Debug.DrawRay(refernce.transform.position, transform.TransformDirection(Vector3.back) * hit.distance, Color.yellow);
+            refernce.transform.position = hit.point;
+            transform.position = new Vector3(transform.position.x,
+                hit.point.y,
+                transform.position.z);
+        }
     }
 }
