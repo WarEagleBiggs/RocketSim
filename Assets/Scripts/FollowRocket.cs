489159e8950e3d8e2b198d26c3806d66e1a112a5
Adam Biggs <wareaglebiggs@gmail.com>
2024-07-04 14:29:03 -0500

    Added a follow rocket script

---
+++ Assets/Scripts/FollowRocket.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRocket : MonoBehaviour
+{
+    public GameObject target;
+    public float lagRate;
+
+    void Update()
+    {
+        this.transform.position = new Vector3(target.transform.position.x,
+            target.transform.position.y + 75,
+            target.transform.position.z - 75);
+    }
+}
